2357de31ceddb78e354d0017511ba9dd4efa7b3a
Bryce <bryce.tan.jw@gmail.com>
2023-11-23 11:34:06 +0800

    main.js renamed to app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// All node modules are imported here
+const express = require("express")
+
+// All app modules are imported here
+
+// Express is initiated here
+app = express()
+
+// Uncaught exception error shuts down server here
+
+// Middleware and Routes used here
