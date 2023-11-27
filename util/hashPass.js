41934f53749784e17976705efbb8ed25e1b8c4ae
Bryce <bryce.tan.jw@gmail.com>
2023-11-27 18:05:57 +0800

    added catch async error

--- util/hashPass.js
+++ util/hashPass.js
@@ -1,13 +1,14 @@
 // require node modules
 const bcrypt = require("bcryptjs")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
 
-exports.hashPass = async (password)=> {
-    try {
-        const salt = await bcrypt.genSalt(10)
-        const hashedpassword = await bcrypt.hash(password, salt) 
-        return hashedpassword
-    } catch (error) {
-        console.error('Error hashing password:', error.message);
-        throw error; // You might want to handle the error or propagate it further
-    }
-}
+exports.hashPass = catchAsyncErrors(async password => {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedpassword = await bcrypt.hash(password, salt)
+    return hashedpassword
+  } catch (error) {
+    console.error("Error hashing password:", error.message)
+    throw error // You might want to handle the error or propagate it further
+  }
+})
