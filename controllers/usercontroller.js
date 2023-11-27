dd552dbf855437743522a89cc763889cfb2c5ec9
Bryce <bryce.tan.jw@gmail.com>
2023-11-27 11:24:53 +0800

    http request URL =/= react render navigation, split post url requests for clarity and simplicity

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -7,7 +7,7 @@ const sendToken = require("../util/JWToken")
 const { checkGroup } = require("../util/checkGroup")
 const { hashPass } = require("../util/hashPass")
 
-// URL received is /login
+// URL get /login
 exports.loginDisplay = async (req, res, next) => {
   // if jwttoken is on browser, react will redirect
   // displays
@@ -17,7 +17,7 @@ exports.loginDisplay = async (req, res, next) => {
   })
 }
 
-// login form submitted
+// URL post /login
 exports.loginForm = async (req, res, next) => {
   const { username, password } = req.body
   console.log(username)
@@ -27,18 +27,19 @@ exports.loginForm = async (req, res, next) => {
   const values = [username]
 
   try {
-    const result = await executeQuery(querystr,values)
+    const result = await executeQuery(querystr, values)
     // if right, send token and user info, react displays home
     if (result[0]) {
       console.log("user login password: ", result[0].password)
       // compares the passwords asynchronously
-      const isMatched = bcrypt.compare(password, result[0].password, (err) => {
+      const isMatched = bcrypt.compare(password, result[0].password, err => {
         if (err) {
-          console.error('Error comparing password:', err);
-          return;
-        }})
+          console.error("Error comparing password:", err)
+          return
+        }
+      })
       if (isMatched) {
-      sendToken(username, 200, res)
+        sendToken(username, 200, res)
       }
     } else {
       // else, react displays same page
@@ -52,7 +53,7 @@ exports.loginForm = async (req, res, next) => {
   }
 }
 
-// URL received is /logout, token will be emptied, then react side will check for token and redirect to login
+// URL get /logout, token will be emptied, then react side will check for token and redirect to login
 exports.logout = async (req, res, next) => {
   res.cookie("token", "none", {
     expires: new Date(Date.now()),
@@ -65,93 +66,89 @@ exports.logout = async (req, res, next) => {
   })
 }
 
-// URL received is /profile/edit
-exports.edit = async (req,res,next)=> {
+// URL get /profile
+exports.profile = async (req, res, next) => {
   // on click, show edit profile component, get username and email to display
   var querystr = `SELECT email FROM users WHERE username = ?`
   const values = [req.user]
 
   try {
     const result = await executeQuery(querystr, values)
-      // return result
-      res.status(200).json({
-        success: true,
-        message: "username and email displayed to for edit screen"
-      })
-    
+    // return result
+    res.status(200).json({
+      success: true,
+      message: "username and email displayed to for edit screen"
+    })
   } catch (error) {
     console.error("Error executing query:", error.message)
   }
 }
 
-// edit form submitted
-exports.editform = async (req,res,next)=> {
+// URL post /profile/edit
+exports.editform = async (req, res, next) => {
   if (req.body.password) {
     // hash password
     req.body.password = await hashPass(req.body.password)
     console.log("hashed password is: ", req.body.password)
   }
-  const fields = Object.keys(req.body);
-  const values = Object.values(req.body);
-  const setClause = fields.map(field => `\`${field}\` = ?`).join(', '); // col1 = ?, col2 = ?...
+  const fields = Object.keys(req.body)
+  const values = Object.values(req.body)
+  const setClause = fields.map(field => `\`${field}\` = ?`).join(", ") // col1 = ?, col2 = ?...
   var querystr = `UPDATE users SET ${setClause} WHERE username = ?`
   values.push(req.user) // ensures that username is bounded by ''
 
   try {
     console.log(querystr)
-    const result = await executeQuery(querystr,values) // replace all the ? with the form values
-      // return result
-      res.status(200).json({
-        success: true,
-        message: "profile edited successfully"
-      })
-    
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    res.status(200).json({
+      success: true,
+      message: "profile edited successfully"
+    })
   } catch (error) {
     console.error("Error executing query:", error.message)
   }
 }
 
-// URL received is /admin
+// URL get /admin
 exports.admin = async (req, res, next) => {
   // check if authorized
-  const authorized = await checkGroup(req.user, '%admin%')
+  const authorized = await checkGroup(req.user, "%admin%")
   if (!authorized) {
     return res.status(400).json({
       success: false,
       message: `unauthorized access`
     })
   }
-    // get all user info
-    var querystr = `SELECT * FROM users`
-    var values = []
-  
+  // get all user info
+  var querystr = `SELECT * FROM users`
+  var values = []
+
+  try {
+    const result = await executeQuery(querystr, values)
+    // get all group info
+    querystr = `SELECT * FROM grouplist`
+
     try {
-      const result = await executeQuery(querystr, values)
-      // get all group info
-      querystr = `SELECT * FROM grouplist`
-    
-      try {
-        const result2 = await executeQuery(querystr, values)
-          // return result
-          res.status(200).json({
-            success: true,
-            message: `admin works, ${result.length} user(s) found, ${result2.length} group(s) found`,
-            data: result
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }      
+      const result2 = await executeQuery(querystr, values)
+      // return result
+      res.status(200).json({
+        success: true,
+        message: `admin works, ${result.length} user(s) found, ${result2.length} group(s) found`,
+        data: result
+      })
     } catch (error) {
       console.error("Error executing query:", error.message)
     }
-    
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
 }
 
-// admin form submitted
-exports.adminForm = async (req,res,next)=> {
+// URL post /admin/edit
+exports.adminEditForm = async (req, res, next) => {
   // check if authorized
-  const authorized = await checkGroup(req.user, '%admin%')
+  const authorized = await checkGroup(req.user, "%admin%")
   if (!authorized) {
     return res.status(400).json({
       success: false,
@@ -163,128 +160,147 @@ exports.adminForm = async (req,res,next)=> {
     req.body.password = await hashPass(req.body.password)
     console.log("hashed password is: ", req.body.password)
   }
-    // check form type
-    if (req.body.formtype === "edit") {
-      var fields = Object.keys(req.body);
-      var values = Object.values(req.body);
 
-      // slice to exclude the field "currUsername"
-      const setClause = fields.slice(0,-2).map(field => `\`${field}\` = ?`).join(', '); 
+  var fields = Object.keys(req.body)
+  var values = Object.values(req.body)
 
-      // converts form body to db appropriate values
-        values = values.slice(0,-2).map(value => ( 
-          value === 'role' ? value.join(',') : 
-          value === 'active' ? 1 : 
-          value === 'disabled' ? 0 : 
-          value
-          )); 
-  
-      var querystr = `UPDATE users SET ${setClause} WHERE username = ?`
-      values.push(req.body.currUsername) // in case username is modified, currUsername will be used to locate the user
-          console.log("values are: ", values)
-      try {
-        const result = await executeQuery(querystr,values) // replace all the ? with the form values
-          // return result
-          res.status(200).json({
-            success: true,
-            message: "user edited successfully"
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }
-    } else if (req.body.formtype === "create") {
-      // check if username is duplicate
-      var querystr = `SELECT username FROM users WHERE username = '${req.body.username}'`
-      try {
-        const result = await executeQuery(querystr,values) // replace all the ? with the form values
-          // return result
-          if (result.length > 0)
-          res.status(400).json({
-            success: false,
-            message: "user already exists"
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }
-      // create new user
-  
-      const requiredFields = ['username', 'password'];
-      
-      var fields = Object.keys(req.body)
-      var values = Object.values(req.body)
-      
-      // Validate that required fields are present
-      if (requiredFields.some(field => !values[fields.indexOf(field)])) {
-        return res.status(400).json({ error: 'Username and password are required fields' });
-      }
-      // converts form body to db appropriate values
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-          console.error('Error generating salt:', err);
-          return;
-        }
-        
-        values = values.map(value => ( 
-          value === 'password' ? bcrypt.hash(password, salt, (err) => {
-            if (err) {
-              console.error('Error hashing password:', err);
-              return;
-            }
-          }) :
-          value === 'role' ? value.join(',') : 
-          value
-          )); 
-        })
+  // slice to exclude the field "currUsername"
+  const setClause = fields
+    .slice(0, -1)
+    .map(field => `\`${field}\` = ?`)
+    .join(", ")
 
-      const placeholders = fields.slice(0,-1).map(field => `\`${field}\` = ?`).join(', ');
-      querystr = `INSERT INTO users SET ${placeholders}`;
-      try {
-        const result = await executeQuery(querystr,values) // replace all the ? with the form values
-          // return result
-          res.status(200).json({
-            success: true,
-            message: "user created successfully"
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }
-    } else if (req.body.formtype === "group") {
-      // check if group is duplicate
-      var querystr = `SELECT * FROM grouplist WHERE groupname = '${req.body.role}'`
-      try {
-        const result = await executeQuery(querystr,values) // replace all the ? with the form values
-          // return result
-          if (result.length > 0)
-          res.status(400).json({
-            success: false,
-            message: "group already exists"
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }
-      // create new group
-      var values = [req.body.role]
-      querystr = `INSERT INTO grouplist VALUES (?)`;
-      try {
-        const result = await executeQuery(querystr,values) // replace all the ? with the form values
-          // return result
-          res.status(200).json({
-            success: true,
-            message: "group created successfully"
-          })
-        
-      } catch (error) {
-        console.error("Error executing query:", error.message)
-      }
-    } else {
-      res.status(400).json({
-        success: false,
-        message: "how did you get here"
-      })
-    }
+  // converts form body to db appropriate values
+  values = values.slice(0, -2).map(value => (value === "role" ? value.join(",") : value === "active" ? 1 : value === "disabled" ? 0 : value))
+
+  var querystr = `UPDATE users SET ${setClause} WHERE username = ?`
+  values.push(req.body.currUsername) // in case username is modified, currUsername will be used to locate the user
+  console.log("values are: ", values)
+  try {
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    res.status(200).json({
+      success: true,
+      message: "user edited successfully"
+    })
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
 }
 
+// URL post /admin/create
+exports.adminCreateForm = async (req, res, next) => {
+  // check if authorized
+  const authorized = await checkGroup(req.user, "%admin%")
+  if (!authorized) {
+    return res.status(400).json({
+      success: false,
+      message: `unauthorized access`
+    })
+  }
+
+  // Validate that required fields are present
+  if (!req.body.password || !req.body.username) {
+    return res.status(400).json({ error: "Username and password are required fields" })
+  }
+
+  // hash password
+  req.body.password = await hashPass(req.body.password)
+  console.log("hashed password is: ", req.body.password)
+
+  // check if username is duplicate
+  var querystr = `SELECT username FROM users WHERE username = '${req.body.username}'`
+  try {
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    if (result.length > 0)
+      res.status(400).json({
+        success: false,
+        message: "user already exists"
+      })
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
+  // create new user
+
+  const requiredFields = ["username", "password"]
+
+  var fields = Object.keys(req.body)
+  var values = Object.values(req.body)
+
+  // converts form body to db appropriate values
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.error("Error generating salt:", err)
+      return
+    }
+
+    values = values.map(value =>
+      value === "password"
+        ? bcrypt.hash(password, salt, err => {
+            if (err) {
+              console.error("Error hashing password:", err)
+              return
+            }
+          })
+        : value === "role"
+        ? value.join(",")
+        : value
+    )
+  })
+
+  const placeholders = fields.map(field => `\`${field}\` = ?`).join(", ")
+  querystr = `INSERT INTO users SET ${placeholders}`
+  try {
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    res.status(200).json({
+      success: true,
+      message: "user created successfully"
+    })
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
+}
+
+// URL post /admin/group
+exports.adminGroupForm = async (req, res, next) => {
+  // check if authorized
+  const authorized = await checkGroup(req.user, "%admin%")
+  if (!authorized) {
+    return res.status(400).json({
+      success: false,
+      message: `unauthorized access`
+    })
+  }
+  // Validate that required fields are present
+  if (!req.body.role) {
+    return res.status(400).json({ error: "Groupname is a required field" })
+  }
+  // check if group is duplicate
+  var querystr = `SELECT * FROM grouplist WHERE groupname = '${req.body.role}'`
+  try {
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    if (result.length > 0)
+      res.status(400).json({
+        success: false,
+        message: "group already exists"
+      })
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
+  // create new group
+  var values = [req.body.role]
+  querystr = `INSERT INTO grouplist VALUES (?)`
+  try {
+    const result = await executeQuery(querystr, values) // replace all the ? with the form values
+    // return result
+    res.status(200).json({
+      success: true,
+      message: "group created successfully"
+    })
+  } catch (error) {
+    console.error("Error executing query:", error.message)
+  }
+}
--- routes/usermgmt.js
+++ routes/usermgmt.js
@@ -2,7 +2,7 @@
 const express = require("express")
 
 // All app modules are imported here
-const { loginDisplay,loginForm, logout, edit, editform, admin, adminForm } = require("../controllers/usercontroller")
+const { loginDisplay, loginForm, logout, edit, editform, admin, adminCreateForm, adminEditForm, adminGroupForm } = require("../controllers/usercontroller")
 const { isAuthenticatedUser } = require("../middleware/auth")
 
 // const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth.js')
@@ -11,13 +11,15 @@ const { isAuthenticatedUser } = require("../middleware/auth")
 const router = express.Router()
 
 // Routes are defined here
-router.route("/login").get(isAuthenticatedUser,loginDisplay) // loggedin(token) ? redirect home : login
+router.route("/login").get(isAuthenticatedUser, loginDisplay) // loggedin(token) ? redirect home : login
 router.route("/login").post(loginForm) // valid(res token) ? redirected url || home : res error -> login
-router.route("/logout").get(isAuthenticatedUser,logout) // loggedin? logout : login
-router.route("/profile").get(isAuthenticatedUser,edit) // loggedin? display edit : login
-router.route("/profile").post(isAuthenticatedUser,editform) // loggedin? submit form -> valid(password rule) -> updated : login
+router.route("/logout").get(isAuthenticatedUser, logout) // loggedin? logout : login
+router.route("/profile").get(isAuthenticatedUser, edit) // loggedin? display edit : login
+router.route("/profile/edit").post(isAuthenticatedUser, editform) // loggedin? submit form -> valid(password rule) -> updated : login
 router.route("/admin").get(isAuthenticatedUser, admin) // loggedin ? admin : redirect login
-router.route("/admin").post(isAuthenticatedUser, adminForm) // loggedin ? submitform -> valid -> updated : login
+router.route("/admin/create").post(isAuthenticatedUser, adminCreateForm) // loggedin ? submitform -> valid -> updated : login
+router.route("/admin/edit").post(isAuthenticatedUser, adminEditForm) // loggedin ? submitform -> valid -> updated : login
+router.route("/admin/group").post(isAuthenticatedUser, adminGroupForm) // loggedin ? submitform -> valid -> updated : login
 
 // Router is exported
 module.exports = router
