7ad0daceae8929910fbd87a635ebc3698d7ef66a
bryce <br0001ei@e.ntu.edu.sg>
2023-11-28 21:48:06 +0800

    created draft of task info api for reference

--- controllers/tmscontroller.js
+++ controllers/tmscontroller.js
@@ -1,10 +1,13 @@
 // require app modules
 const { executeQuery } = require("../config/db")
 
-// URL received is /
-exports.home = Promise.resolve(async (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `home works, user is ${role ? "admin" : "not admin"}`
-  })
-}).catch(next)
+// URL get app/:appacro/tasks/:taskid
+exports.taskinfo = Promise.resolve(async (req, res, next) => {
+  // check if the user has permissions
+  // check which group has permissions
+  // given current task state and app acro, return user groups with permissions
+  // check if user is in groups
+  // given username, return if user groups include groups with permissions: Checkgroup(userid, groupname)
+  // res.json permissions: view || edit
+  // get task info
+})
