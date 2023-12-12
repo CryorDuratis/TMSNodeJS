9051c908a88ee4d91f959f4ef6f0328db03d1795
Bryce <bryce.tan.jw@gmail.com>
2023-12-12 13:26:18 +0800

    editapp done

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -37,7 +37,24 @@ exports.createApp = catchAsyncErrors(async (req, res, next) => {
   })
 })
 // post /app/edit
-exports.editApp = catchAsyncErrors(async (req, res, next) => {})
+exports.editApp = catchAsyncErrors(async (req, res, next) => {
+  const { groupname, token, appacro, ...rest } = req.body
+
+  const fields = Object.keys(rest)
+  const values = Object.values(rest)
+  const setClause = fields.map(field => `\`${field}\` = ?`).join(", ") // col1 = ?, col2 = ?...
+
+  var querystr = `UPDATE application SET ${setClause} WHERE App_Acronym = ?`
+  values.push(appacro)
+
+  console.log("querystr: ", querystr)
+  console.log("values: ", values)
+  const appData = await executeQuery(querystr, values) // replace all the ? with the form values
+  // return result
+  return res.json({
+    success: true
+  })
+})
 // post /app
 exports.getApp = catchAsyncErrors(async (req, res, next) => {
   const { appacro } = req.body
