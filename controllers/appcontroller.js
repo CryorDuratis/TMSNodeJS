585bdb2e629ff2bba9fa5ac76576043a8cf058d3
Bryce <bryce.tan.jw@gmail.com>
2023-12-12 10:26:01 +0800

    create app api done

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -4,7 +4,37 @@ const catchAsyncErrors = require("../errorhandling/catchAsyncErrors")
 
 // post /app/create
 exports.createApp = catchAsyncErrors(async (req, res, next) => {
-  const { acronym, rnumber } = req.body
+  const { acronym, rnumber, startdate = null, enddate = null, permitopen = null, permittodolist = null, permitdoing = null, permitdone = null } = req.body
+
+  // required fields
+  if (!acronym || !rnumber) {
+    return res.json({
+      success: false,
+      message: "required"
+    })
+  }
+
+  // check if appacro is duplicate
+  var querystr = `SELECT App_Acronym FROM application WHERE App_Acronym = ?`
+  var values = [acronym]
+
+  var result = await executeQuery(querystr, values)
+  // return result
+  if (result.length > 0)
+    return res.json({
+      success: false,
+      message: "conflict"
+    })
+
+  // insert
+  querystr = `INSERT INTO application VALUES (?,?,?,?,?,?,?,?,'Project Lead')`
+  values = [acronym, rnumber, startdate, enddate, permitopen, permittodolist, permitdoing, permitdone]
+
+  result = await executeQuery(querystr, values)
+  // return result
+  return res.json({
+    success: true
+  })
 })
 // post /app/edit
 exports.editApp = catchAsyncErrors(async (req, res, next) => {})
